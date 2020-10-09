6be494c3fc05ce2a67388dbea16016eb92d17c47
Jeonghyeon Kim <powergee101@gmail.com>
2020-10-09 20:59:49 +0900

    Edit the solution of 3.26 to display a table

--- Chapter 3/Problem_3_26.m
+++ Chapter 3/Problem_3_26.m
@@ -1,4 +1,5 @@
 clear all;
+format long;
 
 Fun = @(x) 0.5 * exp(2+x) - 40;
 derFun = @(x) 0.5 * exp(2+x);
@@ -6,13 +7,21 @@ derFun = @(x) 0.5 * exp(2+x);
 trueSol = log(80)-2;
 prevNumerSol = NaN;
 numerSol = 4;
+answerTable = zeros(11, 4);
 
 for i = 1:11
     prevNumerSol = numerSol;
     numerSol = numerSol - Fun(numerSol) / derFun(numerSol);
+
+    TRE = abs((trueSol - numerSol) / trueSol);
+    ERE = abs((numerSol - prevNumerSol) / prevNumerSol);
+
+    answerTable(i, 1) = i;
+    answerTable(i, 2) = numerSol;
+    answerTable(i, 3) = TRE;
+    answerTable(i, 4) = ERE;
 end
 
-TRE = abs((trueSol - numerSol) / trueSol);
-ERE = abs((numerSol - prevNumerSol) / prevNumerSol);
-
-fprintf("%d, %d\n", TRE, ERE);
+varNames = {'Iteration', 'Numerical Solution', 'TRE', 'ERE'};
+T = table(answerTable(:, 1), answerTable(:, 2), answerTable(:, 3), answerTable(:, 4), 'VariableNames', varNames);
+disp(T)
