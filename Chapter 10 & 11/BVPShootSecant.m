cef3955e236d5acf3d7ad72c76e4591e262d8137
Jeonghyeon Kim <powergee101@gmail.com>
2020-12-20 23:00:25 +0900

    Improve the accuracy of BVPShootSecant

--- Chapter 10 & 11/BVPShootSecant.m
+++ Chapter 10 & 11/BVPShootSecant.m
@@ -1,31 +1,33 @@
 function [x, y] = BVPShootSecant(fOFx, gOFx, hOFx, a, b, n, Ya, Yb, WL, WH)
     wDiff = @(x, w, y) hOFx(x) - fOFx(x)*w - gOFx(x)*y;
     yDiff = @(x, w, y) w;
+
+    [xL, wL, yL] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WL, Ya);
+    if abs(yL(end) - Yb) < 0.001
+        x = xL;
+        y = yL;
+        return;
+    end
+
+    [xH, wH, yH] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WH, Ya);
+    if abs(yH(end) - Yb) < 0.001
+        x = xH;
+        y = yH;
+        return;
+    end
     
     while true
-        [x1, w1, y1] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WL, Ya);
-        if abs(y1(end) - Yb) < 0.001
-            x = x1;
-            y = y1;
-            break;
-        end
-
-        [x2, w2, y2] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WH, Yb);
-        if abs(y2(end) - Yb) < 0.001
-            x = x2;
-            y = y2;
-            break;
-        end
-
-        WNext = WH - (WL - WH) / (y1(end) - y2(end)) * (y2(end) - Yb);
-        [x3, w3, y3] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WNext, Yb);
-        if abs(y3(end) - Yb) < 0.001
-            x = x3;
-            y = y3;
+        WNext = WH - (WL - WH) / (yL(end) - yH(end)) * (yH(end) - Yb);
+        [xNext, wNext, yNext] = Sys2ODEsRK4(wDiff, yDiff, a, b, (b-a)/n, WNext, Ya);
+        if abs(yNext(end) - Yb) < 0.001
+            x = xNext;
+            y = yNext;
             break;
         end
 
         WL = WH;
         WH = WNext;
+        yL = yH;
+        yH = yNext;
     end
 end
